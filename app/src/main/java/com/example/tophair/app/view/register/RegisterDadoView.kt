198a1fd94ddc685c2c761bbb27e74492cb7a3a4b
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2024-03-26 21:39:08 -0300

    ajuste tela

--- app/src/main/java/com/example/tophair/app/view/register/RegisterDadoView.kt
+++ app/src/main/java/com/example/tophair/app/view/register/RegisterDadoView.kt
@@ -1,55 +1,63 @@
-package com.example.tophair
+package com.example.tophair.app.view.register
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.tophair.R
 import com.example.tophair.app.utils.CustomButton
-import com.example.tophair.app.utils.MarginSpace
-import com.example.tophair.ui.theme.TopHairTheme
-import com.example.tophair.app.view.login.LoginView
-import android.content.Intent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import com.example.tophair.app.utils.CustomRowWithDividers
-import com.example.tophair.app.view.register.RegisterEmailView
+import com.example.tophair.app.utils.MarginSpace
+import com.example.tophair.app.view.login.LoginView
+import com.example.tophair.app.view.register.ui.theme.TopHairTheme
 
-class Login : ComponentActivity() {
+class RegisterDadoView : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -59,7 +67,7 @@ class Login : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen()
+                    RegisterDadoView()
                 }
             }
         }
@@ -68,11 +76,10 @@ class Login : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen(modifier: Modifier = Modifier) {
-
+fun RegisterDadoView( modifier: Modifier = Modifier) {
     val route = LocalContext.current
-    var email by remember { mutableStateOf("") }
-    var senha by remember { mutableStateOf("") }
+    var nomeCompleto by remember { mutableStateOf("") }
+    var telefone by remember { mutableStateOf("") }
 
     BoxWithConstraints(
         modifier = Modifier.fillMaxSize()
@@ -121,7 +128,7 @@ fun LoginScreen(modifier: Modifier = Modifier) {
                     MarginSpace(36.dp)
 
                     Text(stringResource(
-                        R.string.titulo_tela_login),
+                        R.string.titulo_tela_cadastro_dado_pessoal),
                         fontSize = 28.sp,
                         modifier = Modifier
                             .fillMaxWidth()
@@ -134,12 +141,11 @@ fun LoginScreen(modifier: Modifier = Modifier) {
                     MarginSpace(24.dp)
 
                     OutlinedTextField(
-                        value = email,
-                        onValueChange = { email = it },
-                        label = { Text(stringResource(R.string.txt_email)) },
+                        value = nomeCompleto,
+                        onValueChange = { nomeCompleto = it },
+                        label = { Text(stringResource(R.string.txt_nome_compleot)) },
                         singleLine = true,
                         keyboardOptions = KeyboardOptions.Default.copy(
-                            keyboardType = KeyboardType.Email,
                             imeAction = ImeAction.Next
                         ),
                         textStyle = TextStyle(color = Color.Black, fontSize = 18.sp),
@@ -149,8 +155,7 @@ fun LoginScreen(modifier: Modifier = Modifier) {
                             unfocusedBorderColor = Color.Transparent // backgroundColor é definido pelo modificador 'background' abaixo
                         ),
                         modifier = Modifier
-                            .background(Color(0xFFCAC3DC), RoundedCornerShape(32.dp)) // Cor de fundo #cac3dc
-                            .width(280.dp) // Largura específica
+                            .background(Color(0xFFCAC3DC), RoundedCornerShape(32.dp)) // Cor de fundo #cac3dc // Largura específica
                             .height(50.dp)
                             .fillMaxWidth()// Altura específica
                     )
@@ -158,12 +163,12 @@ fun LoginScreen(modifier: Modifier = Modifier) {
                     MarginSpace(16.dp)
 
                     OutlinedTextField(
-                        value = senha,
-                        onValueChange = { senha = it },
-                        label = { Text(stringResource(R.string.txt_senha)) },
+                        value = telefone,
+                        onValueChange = { telefone = it },
+                        label = { Text(stringResource(R.string.txt_telefone)) },
                         singleLine = true,
                         keyboardOptions = KeyboardOptions.Default.copy(
-                            keyboardType = KeyboardType.Password,
+                            keyboardType = KeyboardType.Phone,
                             imeAction = ImeAction.Next
                         ),
                         textStyle = TextStyle(color = Color.Black, fontSize = 18.sp),
@@ -173,39 +178,24 @@ fun LoginScreen(modifier: Modifier = Modifier) {
                             unfocusedBorderColor = Color.Transparent // backgroundColor é definido pelo modificador 'background' abaixo
                         ),
                         modifier = Modifier
-                            .background(Color(0xFFCAC3DC), RoundedCornerShape(32.dp)) // Cor de fundo #cac3dc
-                            .width(280.dp) // Largura específica
+                            .background(Color(0xFFCAC3DC), RoundedCornerShape(32.dp)) // Cor de fundo #cac3dc // Largura específica
                             .height(50.dp)
                             .fillMaxWidth()// Altura específica
                     )
 
                     MarginSpace(16.dp)
 
-                    CustomButton(stringResource(R.string.btn_txt_login), onClick= {
+                    CustomButton(stringResource(R.string.btn_txt_continue), onClick= {
                         val loginView = Intent(route, LoginView::class.java)
 
                         route.startActivity(loginView)
+
                     })
 
                     MarginSpace(16.dp)
 
-                    TextButton(
-                        onClick = {
-                            // TODO: Implemente a lógica do que deve acontecer quando o texto for clicado
-                        },
-                        modifier = Modifier.padding(8.dp) // Espaçamento ao redor do botão
-                    ) {
-                        Text(
-                            text = stringResource(R.string.txt_senha_reset),
-                            fontSize = 14.sp,
-                            color = Color.White,
-                            fontWeight = FontWeight.Bold
-                        )
-                    }
                 }
             }
-
-
 
             Column(
                 modifier = Modifier
@@ -228,8 +218,8 @@ fun LoginScreen(modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun Login1() {
+fun GreetingPreview4() {
     TopHairTheme {
-        LoginScreen()
+        RegisterDadoView()
     }
 }
