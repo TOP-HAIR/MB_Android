c49fe032b4b44535e57028f33e9ed686538f3ae6
GabrielTK <110927351+GabrielKenjiTomariKohatu@users.noreply.github.com>
2024-06-14 09:44:13 -0300

    Update build.gradle.kts
    Ajuste para que no defaultConfig o minSdk para 25 para que o app suporte ser instalado em dispositivos android para teste

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.tophair"
-        minSdk = 34
+        minSdk = 25
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
